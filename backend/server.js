f877db5172eb99f2845062a6a9e09e7ad803e485
Adnan Musil <adnan.musil@dimensionalsys.com>
2023-01-31 20:10:34 +0500

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import products from './data/products.js'
+import connectDB from './config/db.js'
+import colors from 'colors'
 
 const app = express()
+
+//mongo db
+connectDB()
 
 dotenv.config()
 
@@ -21,4 +26,4 @@ app.get('/api/products/:id',(req,res)=>{
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
