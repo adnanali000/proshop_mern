bc286138e0491857c0aa1ba0a2716a86c7c93544
Adnan Musil <adnan.musil@dimensionalsys.com>
2023-01-31 18:58:16 +0500

    fetch data from backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import {Row,Col,Image,ListGroup,Card,Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import Products from '../products'
-import products from '../products'
+import axios from 'axios'
 
 function ProductScreen({match}) {
-    const product = products.find(p => p._id === match.params.id)
-    console.log(product)
+    const [product,setProduct] = useState({})
+   
+    useEffect(()=>{
+        const fetchProducts = async ()=>{
+          const {data} = await axios.get(`/api/products/${match.params.id}`)
+          setProduct(data)
+        }
+    
+        fetchProducts()
+    
+      },[])
 
   return (
     <>
